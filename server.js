5c2dcf63191836d575a96cd5516bc2e7ca6b7192
Rygard <33646871+Rygard54@users.noreply.github.com>
2019-05-28 11:17:51 -0700

    changes of the last part of chapter 9

--- server.js
+++ server.js
@@ -63,11 +63,18 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:'Projects'
+    });
+});
+
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage: 'Unable to handle request'
     })
 });
+
 app.listen(port, ()=>{
     console.log(`server is up on port ${port}`);
 });
